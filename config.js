1a41fd8a94cb4f4ac5dd10c4eb9a98831576d165
Kingamir <nafaskingamir@gmail.com>
2024-10-02 20:42:59 +0430

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=TUcwlYTJ#j0pK5MsiVYFDfqjl47rwf3P3lFnL2JacL08TJR1dFLM",
+MONGODB: process.env.MONGODB || "mongodb+srv://enayatullahzakhil0:8IqqtYC9vMMSTx50@cluster001.ierzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster001",//enter mongo db url
 };
